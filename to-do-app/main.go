f2a33b7363bd61c22765bd88bde7f848144a74ad
sujalcharati <sujalc024@gmail.com>
2025-07-15 19:41:12 +0530

    added /GET

--- to-do-app/main.go
+++ to-do-app/main.go
@@ -6,54 +6,65 @@ import (
 	"net/http"
 )
 
-
 type task struct {
-	Id string `json:"id"`
-	Title string `json:"title"`
+	Id          string `json:"id"`
+	Title       string `json:"title"`
 	Description string `json:"description"`
-	Status string `json:"status"`
+	Status      string `json:"status"`
 }
 
-
 var (
-	Task []task
+	Task   []task
 	taskID int = 1
 )
 
-func createNewTask(w http.ResponseWriter,r *http.Request){
-      
-	if r.Method != http.MethodPost {
-		http.Error(w,"invalid request method",http.StatusNotAcceptable)
+func getTasks(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != http.MethodGet {
+		http.Error(w, "invalid req method", http.StatusNotAcceptable)
 		return
+	}
 
-	} 
-      var newTask task
-	  err := json.NewDecoder(r.Body).Decode(&newTask)
-
-	  if err !=nil {
-          http.Error(w,"invalid body",http.StatusBadRequest)
-		  return
-	  }
-
-	  if newTask.Title == "" || newTask.Description == ""{
-		http.Error(w, "write down the title and desc",http.StatusBadRequest)
-		return
-	  } 
-
-
-	  taskID++
-	  newTask.Id = fmt.Sprintf("%d", taskID)
-	  taskID++
-	  Task = append(Task, newTask)
-	  
-	  w.Header().Set("Content-Type","application/json")
-	  json.NewEncoder(w).Encode(newTask)
+	
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(Task)
 
 }
 
-func main(){
+func createNewTask(w http.ResponseWriter, r *http.Request) {
 
-	http.HandleFunc("/tasks",createNewTask)
+	if r.Method != http.MethodPost {
+		http.Error(w, "invalid request method", http.StatusNotAcceptable)
+		return
+
+	}
+	var newTask task
+	err := json.NewDecoder(r.Body).Decode(&newTask)
+
+	if err != nil {
+		http.Error(w, "invalid body", http.StatusBadRequest)
+		return
+	}
+
+	if newTask.Title == "" || newTask.Description == "" {
+		http.Error(w, "write down the title and desc", http.StatusBadRequest)
+		return
+	}
+
+	taskID++
+	newTask.Id = fmt.Sprintf("%d", taskID)
+	taskID++
+	Task = append(Task, newTask)
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(newTask)
+
+}
+
+func main() {
+
+	http.HandleFunc("/tasks", createNewTask)
+	http.HandleFunc("/gettasks", getTasks)
 	fmt.Print(" server is running on port 8080\n")
 	http.ListenAndServe(":8080", nil)
 }
