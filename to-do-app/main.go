2d2e49683f0712b0b3d2a5d741f071639dd2c8de
sujalcharati <sujalc024@gmail.com>
2025-07-16 17:34:05 +0530

    GET  /gettasks/{id} & PUT /tasks

--- to-do-app/main.go
+++ to-do-app/main.go
@@ -18,6 +18,58 @@ var (
 	taskID int = 1
 )
 
+func updateTask(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != http.MethodPut {
+		http.Error(w, " invalid request method", http.StatusBadRequest)
+		return
+	}
+
+	id := r.URL.Path[len("/tasks/"):]
+
+	for i, t := range Task {
+		if t.Id == id {
+
+			var updated_task task
+
+			err := json.NewDecoder(r.Body).Decode(&updated_task)
+
+			if err != nil {
+				http.Error(w, " invalid body", http.StatusBadRequest)
+				return
+			}
+			Task[i].Title = updated_task.Title
+			Task[i].Description = updated_task.Description
+			Task[i].Status = updated_task.Status
+
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(Task[i])
+			return
+
+		}
+	}
+	http.Error(w, "task not found", http.StatusNotFound)
+}
+
+func getTaskByID(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != http.MethodGet {
+		http.Error(w, " invalid request", http.StatusBadRequest)
+		return
+	}
+
+	id := r.URL.Path[len("/gettasks/"):]
+
+	for _, task := range Task {
+		if task.Id == id {
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(task)
+			return
+		}
+	}
+	http.Error(w, "task not found", http.StatusNotFound)
+}
+
 func getTasks(w http.ResponseWriter, r *http.Request) {
 
 	if r.Method != http.MethodGet {
@@ -25,7 +77,6 @@ func getTasks(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(Task)
 
@@ -53,7 +104,6 @@ func createNewTask(w http.ResponseWriter, r *http.Request) {
 
 	taskID++
 	newTask.Id = fmt.Sprintf("%d", taskID)
-	taskID++
 	Task = append(Task, newTask)
 
 	w.Header().Set("Content-Type", "application/json")
@@ -65,6 +115,8 @@ func main() {
 
 	http.HandleFunc("/tasks", createNewTask)
 	http.HandleFunc("/gettasks", getTasks)
+	http.HandleFunc("/gettasks/", getTaskByID)
+	http.HandleFunc("/tasks/", updateTask)
 	fmt.Print(" server is running on port 8080\n")
 	http.ListenAndServe(":8080", nil)
 }
